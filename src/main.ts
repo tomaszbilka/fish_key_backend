4be06439b54b297eedb47ee8fad3e0c856071b98
tomaszbilka <bila4@op.pl>
2024-09-13 20:21:21 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupApp } from './setup-app';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { exportSchemaToFile } from './utils/save-swagger-to-file';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,16 @@ async function bootstrap() {
     origin: configService.get<string>('CORS_ORIGIN'),
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Fishkey API')
+    .setDescription('API for fishkey')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  exportSchemaToFile(document);
+
   const PORT = configService.get<number>('PORT');
   await app.listen(PORT || 3000);
 }
