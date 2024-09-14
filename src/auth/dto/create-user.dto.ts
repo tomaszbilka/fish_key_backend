38c76b44075121e139efe8963251896a37a60aa7
tomaszbilka <bila4@op.pl>
2024-09-14 14:02:00 +0200

    add swagger responses dto

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   readonly email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(6)
   readonly password: string;
--- src/auth/dto/user-serialize.dto.ts
+++ src/auth/dto/user-serialize.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class UserSerializeDto {
+  @ApiProperty()
   @Expose()
   id: number;
 
+  @ApiProperty()
   @Expose()
   email: string;
 }
